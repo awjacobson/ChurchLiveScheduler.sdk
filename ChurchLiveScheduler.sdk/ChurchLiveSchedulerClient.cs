977212d47135ed59748e37fb4634aad01e73d869
Aaron Jacobson <Aaron.Jacobson@ayahealthcare.com>
2026-09-21 17:39:37 -0500

    Switch to Source Generation to support Native AOT and trimmed apps

--- ChurchLiveScheduler.sdk/ChurchLiveSchedulerClient.cs
+++ ChurchLiveScheduler.sdk/ChurchLiveSchedulerClient.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using ChurchLiveScheduler.sdk.Models;
 
 namespace ChurchLiveScheduler.sdk;
@@ -19,29 +20,29 @@ public sealed class ChurchLiveSchedulerClient : IChurchLiveSchedulerClient
         _client.GetStringAsync("/api/GetCurrentTime", cancellationToken);
 
     public Task<GetNextResponse?> GetNextAsync(CancellationToken cancellationToken = default) =>
-        _client.GetFromJsonAsync<GetNextResponse>("/api/GetNext", cancellationToken);
+        _client.GetFromJsonAsync("/api/GetNext", SourceGenerationContext.Default.GetNextResponse, cancellationToken);
 
     public Task<GetAllResponse?> GetAllAsync(CancellationToken cancellationToken = default) =>
-        _client.GetFromJsonAsync<GetAllResponse>("/api/Getall", cancellationToken);
+        _client.GetFromJsonAsync("/api/Getall", SourceGenerationContext.Default.GetAllResponse, cancellationToken);
 
-    public Task<IReadOnlyList<SeriesDto>?> GetSeriesListAsync(CancellationToken cancellationToken = default) =>
-        _client.GetFromJsonAsync<IReadOnlyList<SeriesDto>?>("/api/series", cancellationToken);
+    public Task<IEnumerable<SeriesDto>?> GetSeriesListAsync(CancellationToken cancellationToken = default) =>
+        _client.GetFromJsonAsync("/api/series", SourceGenerationContext.Default.IEnumerableSeriesDto, cancellationToken);
 
     public Task<SeriesDto?> GetSeriesDetailAsync(int id, CancellationToken cancellationToken = default) =>
-        _client.GetFromJsonAsync<SeriesDto?>($"/api/series/{id}", cancellationToken);
+        _client.GetFromJsonAsync($"/api/series/{id}", SourceGenerationContext.Default.SeriesDto, cancellationToken);
 
     public Task<UpdateSeriesResponse?> UpdateSeriesAsync(int id, CancellationToken cancellationToken = default) =>
-        _client.GetFromJsonAsync<UpdateSeriesResponse?>($"/api/series/{id}", cancellationToken);
+        _client.GetFromJsonAsync($"/api/series/{id}", SourceGenerationContext.Default.UpdateSeriesResponse, cancellationToken);
 
-    public Task<IReadOnlyList<CancellationDto>?> GetCancellationListAsync(int seriesId, CancellationToken cancellationToken = default) =>
-        _client.GetFromJsonAsync<IReadOnlyList<CancellationDto>?>($"/api/series/{seriesId}/cancellations", cancellationToken);
+    public Task<IEnumerable<CancellationDto>?> GetCancellationListAsync(int seriesId, CancellationToken cancellationToken = default) =>
+        _client.GetFromJsonAsync($"/api/series/{seriesId}/cancellations", SourceGenerationContext.Default.IEnumerableCancellationDto, cancellationToken);
 
     public async Task<CancellationDto?> CreateCancellationAsync(int seriesId, CreateCancellationRequest request, CancellationToken cancellationToken = default)
     {
-        var response = await _client.PostAsJsonAsync($"/api/series/{seriesId}/cancellations", request, cancellationToken);
+        var response = await _client.PostAsJsonAsync($"/api/series/{seriesId}/cancellations", request, SourceGenerationContext.Default.CreateCancellationRequest, cancellationToken);
         if (response.IsSuccessStatusCode)
         {
-            return await response.Content.ReadFromJsonAsync<CancellationDto>(cancellationToken: cancellationToken) ?? new CancellationDto();
+            return await response.Content.ReadFromJsonAsync(SourceGenerationContext.Default.CancellationDto, cancellationToken) ?? new CancellationDto();
         }
         return null;
     }
@@ -49,10 +50,10 @@ public sealed class ChurchLiveSchedulerClient : IChurchLiveSchedulerClient
     public async Task<UpdateCancellationResponse?> UpdateCancellationAsync(
         int seriesId, UpdateCancellationRequest request, CancellationToken cancellationToken = default)
     {
-        var response = await _client.PutAsJsonAsync($"/api/series/{seriesId}/cancellations", request, cancellationToken);
+        var response = await _client.PutAsJsonAsync($"/api/series/{seriesId}/cancellations", request, SourceGenerationContext.Default.UpdateCancellationRequest, cancellationToken);
         if (response.IsSuccessStatusCode)
         {
-            return await response.Content.ReadFromJsonAsync<UpdateCancellationResponse>(cancellationToken: cancellationToken) ?? new UpdateCancellationResponse();
+            return await response.Content.ReadFromJsonAsync(SourceGenerationContext.Default.UpdateCancellationResponse, cancellationToken) ?? new UpdateCancellationResponse();
         }
         return null;
     }
@@ -62,30 +63,30 @@ public sealed class ChurchLiveSchedulerClient : IChurchLiveSchedulerClient
         var response = await _client.DeleteAsync($"/api/series/{seriesId}/cancellations/{cancellationId}", cancellationToken);
         if (response.IsSuccessStatusCode)
         {
-            return await response.Content.ReadFromJsonAsync<DeleteCancellationResponse>(cancellationToken: cancellationToken) ?? new DeleteCancellationResponse();
+            return await response.Content.ReadFromJsonAsync(SourceGenerationContext.Default.DeleteCancellationResponse, cancellationToken);
         }
         return null;
     }
 
-    public Task<IReadOnlyList<SpecialDto>?> GetSpecialsAsync(CancellationToken cancellationToken = default) =>
-        _client.GetFromJsonAsync<IReadOnlyList<SpecialDto>?>("/api/specials", cancellationToken);
+    public Task<IEnumerable<SpecialDto>?> GetSpecialsAsync(CancellationToken cancellationToken = default) =>
+        _client.GetFromJsonAsync<IEnumerable<SpecialDto>?>("/api/specials", cancellationToken);
 
     public async Task<CreateSpecialResponse?> CreateSpecialAsync(CreateSpecialRequest request, CancellationToken cancellationToken = default)
     {
-        var response = await _client.PostAsJsonAsync($"/api/specials", request, cancellationToken);
+        var response = await _client.PostAsJsonAsync($"/api/specials", request, SourceGenerationContext.Default.CreateSpecialRequest, cancellationToken);
         if (response.IsSuccessStatusCode)
         {
-            return await response.Content.ReadFromJsonAsync<CreateSpecialResponse>(cancellationToken: cancellationToken);
+            return await response.Content.ReadFromJsonAsync(SourceGenerationContext.Default.CreateSpecialResponse, cancellationToken);
         }
         return null;
     }
 
     public async Task<UpdateSpecialResponse?> UpdateSpecialAsync(int specialId, UpdateSpecialRequest request, CancellationToken cancellationToken = default)
     {
-        var response = await _client.PutAsJsonAsync($"/api/specials/{specialId}", request, cancellationToken);
+        var response = await _client.PutAsJsonAsync($"/api/specials/{specialId}", request, SourceGenerationContext.Default.UpdateSpecialRequest, cancellationToken);
         if (response.IsSuccessStatusCode)
         {
-            return await response.Content.ReadFromJsonAsync<UpdateSpecialResponse>(cancellationToken: cancellationToken);
+            return await response.Content.ReadFromJsonAsync(SourceGenerationContext.Default.UpdateSpecialResponse, cancellationToken);
         }
         return null;
     }
@@ -95,8 +96,28 @@ public sealed class ChurchLiveSchedulerClient : IChurchLiveSchedulerClient
         var response = await _client.DeleteAsync($"/api/specials/{specialId}", cancellationToken);
         if (response.IsSuccessStatusCode)
         {
-            return await response.Content.ReadFromJsonAsync<DeleteSpecialResponse>(cancellationToken: cancellationToken);
+            return await response.Content.ReadFromJsonAsync(SourceGenerationContext.Default.DeleteSpecialResponse, cancellationToken);
         }
         return null;
     }
 }
+
+[JsonSourceGenerationOptions(PropertyNameCaseInsensitive = true)]
+[JsonSerializable(typeof(GetNextResponse))]
+[JsonSerializable(typeof(GetAllResponse))]
+[JsonSerializable(typeof(IEnumerable<SeriesDto>))]
+[JsonSerializable(typeof(SeriesDto))]
+[JsonSerializable(typeof(UpdateSeriesResponse))]
+[JsonSerializable(typeof(CreateCancellationRequest))]
+[JsonSerializable(typeof(IEnumerable<CancellationDto>))]
+[JsonSerializable(typeof(CancellationDto))]
+[JsonSerializable(typeof(UpdateCancellationRequest))]
+[JsonSerializable(typeof(UpdateCancellationResponse))]
+[JsonSerializable(typeof(DeleteCancellationResponse))]
+[JsonSerializable(typeof(IEnumerable<SpecialDto>))]
+[JsonSerializable(typeof(CreateSpecialRequest))]
+[JsonSerializable(typeof(CreateSpecialResponse))]
+[JsonSerializable(typeof(UpdateSpecialRequest))]
+[JsonSerializable(typeof(UpdateSpecialResponse))]
+[JsonSerializable(typeof(DeleteSpecialResponse))]
+internal partial class SourceGenerationContext : JsonSerializerContext { }
--- ChurchLiveScheduler.sdk/IChurchLiveSchedulerClient.cs
+++ ChurchLiveScheduler.sdk/IChurchLiveSchedulerClient.cs
@@ -8,16 +8,16 @@ public interface IChurchLiveSchedulerClient
     Task<GetNextResponse?> GetNextAsync(CancellationToken cancellationToken = default);
     Task<GetAllResponse?> GetAllAsync(CancellationToken cancellationToken = default);
 
-    Task<IReadOnlyList<SeriesDto>?> GetSeriesListAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<SeriesDto>?> GetSeriesListAsync(CancellationToken cancellationToken = default);
     Task<SeriesDto?> GetSeriesDetailAsync(int id, CancellationToken cancellationToken = default);
     Task<UpdateSeriesResponse?> UpdateSeriesAsync(int id, CancellationToken cancellationToken = default);
 
-    Task<IReadOnlyList<CancellationDto>?> GetCancellationListAsync(int seriesId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<CancellationDto>?> GetCancellationListAsync(int seriesId, CancellationToken cancellationToken = default);
     Task<CancellationDto?> CreateCancellationAsync(int seriesId, CreateCancellationRequest request, CancellationToken cancellationToken = default);
     Task<UpdateCancellationResponse?> UpdateCancellationAsync(int seriesId, UpdateCancellationRequest request, CancellationToken cancellationToken = default);
     Task<DeleteCancellationResponse?> DeleteCancellationAsync(int seriesId, int cancellationId, CancellationToken cancellationToken = default);
 
-    Task<IReadOnlyList<SpecialDto>?> GetSpecialsAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<SpecialDto>?> GetSpecialsAsync(CancellationToken cancellationToken = default);
     Task<CreateSpecialResponse?> CreateSpecialAsync(CreateSpecialRequest request, CancellationToken cancellationToken = default);
     Task<UpdateSpecialResponse?> UpdateSpecialAsync(int specialId, UpdateSpecialRequest request, CancellationToken cancellationToken = default);
     Task<DeleteSpecialResponse?> DeleteSpecialAsync(int specialId, CancellationToken cancellationToken = default);
